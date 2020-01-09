2594bf56d6113da0eb0ac83c1d1fea43fa098046
Sebastian <Seburath@gmail.com>
2020-01-09 01:32:46 -0500

    Update random_trade.py

--- algos/random_trade.py
+++ algos/random_trade.py
@@ -1,8 +1,11 @@
 import cyberhead as ch
 import random
+from time import sleep
 
 broker = ch.brokers.interactive
 random_trade = random.choice([True, False])
 
-if random_trade:
-  broker.buy('1000', 'TSLA')
+while True:
+  if random_trade:
+    broker.buy('1000', 'TSLA')
+  sleep(1000000)
