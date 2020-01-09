900b12e4b5524bd7b9afc0f4522729d19ee3e17d
Sebastian <Seburath@gmail.com>
2020-01-09 01:30:57 -0500

    Update trade_on_signal.py

--- algos/trade_on_signal.py
+++ algos/trade_on_signal.py
@@ -1,7 +1,12 @@
 import cyberhead as ch
+from time import sleep
 
 signal = ch.databases.redis.signals.position
 broker = ch.brokers.interactive
 
-if signal[-1] == 'Long':
-  broker.buy('1000', 'TSLA')
+while True:
+  if signal[-1] == 'Long':
+    broker.buy('1000', 'TSLA')
+  if signal[-1] == 'Short':
+    broker.sell('1000', 'TSLA')
+  sleep(1000)
