c696746807724d453b02f16aa41c93fc4687d893
Luis <luis@luisem.co>
2020-02-07 20:09:29 -0300

    Brokers Structure

---
+++ API/brokers/alpaca.py
@@ -0,0 +1,6 @@
+class Alpaca(object):
+	"""Alpaca Markets API Wrapper"""
+	def __init__(self, arg):
+		super(Alpaca, self).__init__()
+		self.arg = arg
+		
---
+++ API/brokers/ameritrade.py
@@ -0,0 +1,14 @@
+class Ameritrade(object):
+	"""TD Ameritrade API Wrapper"""
+	def __init__(self, arg):
+		super(Ameritrade, self).__init__()
+		self.arg = arg
+	
+	def refresh_token():
+		pass
+
+	def check_authentication():
+		pass
+
+	def place_order():
+		pass
