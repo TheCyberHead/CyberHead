3ce54e7cce9705677231a8fad3c1efeb81be8ec0
Luis <luis@luisem.co>
2020-02-19 03:29:20 -0300

    Perform Strategy

---
+++ API/strategies/strategy_one.py
@@ -0,0 +1,27 @@
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
+
+from backtesting.test import SMA, GOOG
+
+
+class CrossGOOG(Strategy):
+    def init(self):
+        Close = self.data.Close
+        self.ma1 = self.I(SMA, Close, 10)
+        self.ma2 = self.I(SMA, Close, 20)
+
+    def next(self):
+        if crossover(self.ma1, self.ma2):
+            self.buy()
+        elif crossover(self.ma2, self.ma1):
+            self.sell()
+
+
+def run_backtest():
+	bt = Backtest(GOOG, CrossGOOG, cash=10000, commission=.002)
+	output = bt.run()
+	print(output)
+	bt.plot(open_browser=False, plot_width=700)
+
+if __name__ == '__main__':
+	run_backtest()
