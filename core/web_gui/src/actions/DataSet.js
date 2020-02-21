b7c9e59d6d83feb501fab7908ccef5b817b55e60
Seburath <seburath@gmail.com>
2020-02-20 20:55:04 -0500

    reordering

--- core/web_gui/src/actions/DataSet.js
+++
@@ -1,8 +0,0 @@
-async function getDatasets(){
-	let response = await fetch('http://localhost:5000/datasets')
-	let data = await response.json()
-	return data
-}
-
-
-export default getDatasets;
--- core/web_gui/src/actions/getStrategies.js
+++
@@ -1,8 +0,0 @@
-async function getStrategies(strategy){
-	let response = await fetch(`http://localhost:5000/get_strategies`)
-	let data = await response.json()
-	return data
-}
-
-
-export default getStrategies;
