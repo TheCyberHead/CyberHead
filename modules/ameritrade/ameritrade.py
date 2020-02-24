1d59bb4b58644f424c74b1b5cf799cd0a10741e8
Luis <luis@luisem.co>
2020-02-24 06:09:35 -0300

    Brokers import

--- modules/ameritrade/ameritrade.py
+++ modules/ameritrade/ameritrade.py
@@ -1,14 +1,79 @@
-class Ameritrade(object):
-	"""TD Ameritrade API Wrapper"""
-	def __init__(self, arg):
-		super(Ameritrade, self).__init__()
-		self.arg = arg
-	
-	def refresh_token():
-		pass
+import requests
+import json
 
-	def check_authentication():
-		pass
+def get_account_details(token):
+	headers = {
+		"Authorization": f"Bearer {token}",
+	}
+	req = requests.get('https://api.tdameritrade.com/v1/accounts', headers=headers)
+	return req.json()
 
-	def place_order():
-		pass
+
+def place_order_tda(action, quantity, ticker, account_id, bearer):
+	headers = {
+		'Authorization': 'Bearer {}'.format(bearer)
+	}
+	payload = {
+	  "orderType": "MARKET",
+	  "session": "NORMAL",
+	  "duration": "DAY",
+	  "orderStrategyType": "SINGLE",
+	  "orderLegCollection": [
+	    {
+	      "instruction": action,
+	      "quantity": quantity,
+	      "instrument": {
+	        "symbol": ticker,
+	        "assetType": "EQUITY"
+	      }
+	    }
+	  ]
+	}
+	url = 'https://api.tdameritrade.com/v1/accounts/{}/orders'.format(account_id)	
+	req = requests.post(url, headers=headers, json=payload)
+	return req.status_code
+
+def place_limit_order_tda(action, entry_price, exit_price, ticker, quantity, order_type, bearer, account_id):
+	headers = {
+		'Authorization': 'Bearer {}'.format(bearer)
+	}
+	exit_action = "Sell" if action == 'Buy' else "Buy"
+	payload = {
+	  "orderType": "LIMIT",
+	  "session": "NORMAL",
+	  "price": entry_price,
+	  "duration": order_type,
+	  "orderStrategyType": "TRIGGER",
+	  "orderLegCollection": [
+	    {
+	      "instruction": action,
+	      "quantity": quantity,
+	      "instrument": {
+	        "symbol": ticker,
+	        "assetType": "EQUITY"
+	      }
+	    }
+	  ],
+	  "childOrderStrategies": [
+	    {
+	      "orderType": "LIMIT",
+	      "session": "NORMAL",
+	      "price": exit_price,
+	      "duration": order_type,
+	      "orderStrategyType": "SINGLE",
+	      "orderLegCollection": [
+	        {
+	          "instruction": exit_action,
+	          "quantity": quantity,
+	          "instrument": {
+	            "symbol": ticker,
+	            "assetType": "EQUITY"
+	          }
+	        }
+	      ]
+	    }
+	  ]
+	}
+	url = 'https://api.tdameritrade.com/v1/accounts/{}/orders'.format(account_id)
+	req = requests.post(url, headers=headers, json=payload)
+	return req.status_code
---
+++ modules/coinbase/coinbase.py
@@ -0,0 +1,36 @@
+import cbpro
+from base64 import b64encode
+
+class Coin:
+	def __init__(self, API_KEY, API_SECRET, API_PASS, ENV_URL="https://pro.coinbase.com"):
+		self.API_KEY = API_KEY
+		self.API_SECRET = API_SECRET
+		self.API_PASS = API_PASS
+		self.ENV_URL = ENV_URL
+		self.client = cbpro.AuthenticatedClient(self.API_KEY, self.API_SECRET, self.API_PASS, api_url=self.ENV_URL)
+
+	def place_market(self, action, ticker, amount):
+		order = self.client.place_market_order(
+				product_id=ticker,
+				side=action,
+				funds=amount
+			)
+		return place_market
+
+	def place_limit_order(self, action, ticker, entry_price, size):
+		print('ENTRY ORDER COINBASE')
+		entry_order = self.client.place_limit_order(product_id=ticker, 
+							side=action, 
+							price=entry_price, 
+							size=size)
+		print(entry_order)
+		return entry_order
+
+	def get_accounts(self):
+		return self.client.get_accounts()
+
+	def orders(self):
+		return self.client.get_orders()
+
+	def fills(self):
+		return self.client.get_fills()
