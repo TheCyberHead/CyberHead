7c70c65c3c9abdf1cdee8ba9e82395bac3aae28e
Luis <luis@luisem.co>
2020-02-25 04:52:00 -0300

    Delete and move backup to branch

--- old/brokers/interactive_brokers/normalize.py
+++
@@ -1,8 +0,0 @@
-from database import Scraped, Historical
-
-symbols = [(record.symbol) for record in Scraped.select()]
-
-for ticker in symbols:
-	records = Historical.select().where(Historical.ticker==ticker)
-	print(ticker)
-	print(len(records))
