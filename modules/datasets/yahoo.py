b86aa122d868c6a876e4f934e3de0580b537f29d
Luis <luis@luisem.co>
2020-02-26 21:05:29 -0300

    Fetch last minute data

--- modules/datasets/yahoo.py
+++ modules/datasets/yahoo.py
@@ -1,9 +1,8 @@
 import yfinance as yf
-from sqlalchemy import create_engine 
 from pandas_datareader import data as pdr
 
 yf.pdr_override()
 
-def download_historical(ticker: str, start_date: str, end_date: str, interval: str):
-	data = pdr.get_data_yahoo(ticker, start=start_date, end=end_date, interval = interval)
+def download_historical(ticker: str, period: str, interval: str):
+	data = pdr.get_data_yahoo(ticker, period=period, interval = interval)
 	return data
--- recurrent.py
+++ recurrent.py
@@ -1,5 +1,21 @@
 from database import DataSet, engine
 from modules.datasets import yahoo
+import numpy as np
+import pandas
 
-data = yahoo.download_historical('AMZN', "2018-03-01", "2020-02-08", "1h")
-data.to_csv('tmp/test.csv')
+def fetchDataSet(ticker: str, period: str, interval: str, dataset_id: int):
+	data = yahoo.download_historical(ticker, period, interval)
+	data.to_csv('tmp/test.csv')
+	read_export = pandas.read_csv('tmp/test.csv')
+	read_export.drop('Adj Close',axis=1, inplace=True)
+	read_export.columns = ['datetime', 'open_price', 'high_price', 'low_price', 'closing_price', 'volume']
+	read_export['dataset_id'] = 12422
+	read_export.to_sql('history', con=engine, if_exists='append', index = False)
+	print(read_export)
+
+
+
+if __name__ == '__main__':
+	symbols = ['NFLX', 'AMZN', 'GS', 'MS', 'GOOG', 'FB', 'TSLA', 'WM', 'UBER', 'CRM', 'F', 'T', 'KO']
+	for symbol in symbols:
+		fetchDataSet(symbol, "max", "1d", 2212)
