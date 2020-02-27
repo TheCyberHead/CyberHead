234101ef53c207367b04edf777e975865dafb949
Luis <luis@luisem.co>
2020-02-27 00:32:25 -0300

    Str interpolation

--- recurrent.py
+++ recurrent.py
@@ -6,9 +6,9 @@ import pandas
 def allTimeFetch(ticker: str, period: str, interval: str, dataset_id: int):
 	#xfetchDataSet(symbol, "max", "1d", 2212)
 	data = yahoo.download_historical(ticker, period, interval)
-	data.to_csv('tmp/test.csv')
-	read_export = pandas.read_csv('tmp/test.csv')
+	data.to_csv('tmp/{}.csv'.format(ticker))
+	read_export = pandas.read_csv('tmp/{}.csv'.format(ticker))
 	read_export.drop('Adj Close',axis=1, inplace=True)
 	read_export.columns = ['datetime', 'open_price', 'high_price', 'low_price', 'closing_price', 'volume']
-	read_export['dataset_id'] = 12422
+	read_export['dataset_id'] = dataset_id
 	read_export.to_sql('history', con=engine, if_exists='append', index = False)
