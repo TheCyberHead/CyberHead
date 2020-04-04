a1938d1b99c56749fff258f59f820a09de91ed5e
seburath <seburath@gmail.com>
2020-04-04 02:07:21 -0500

    setup stuff gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(name='cyberhead',
+version='1.0',
+description='modular automated trading',
+url='cyberhead.uk',
+author='cyberhead',
+author_email='info@cyberhead.uk',
+license='MIT',
+packages=['cyberhead'],
+zip_safe=False)
