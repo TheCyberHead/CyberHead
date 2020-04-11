f5b6a0d3eddcccd9fabfd2e4b274d9437073ba50
seburath <seburath@gmail.com>
2020-04-10 23:12:18 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 setup(name='cyberhead',
-version='1.0',
-description='modular automated trading',
-url='cyberhead.uk',
-author='cyberhead',
-author_email='info@cyberhead.uk',
-license='MIT',
-packages=['cyberhead'],
-zip_safe=False)
+    version='1.0',
+    description='modular automated trading',
+    url='cyberhead.uk',
+    author='cyberhead',
+    author_email='info@cyberhead.uk',
+    license='MIT',
+    packages=find_packages('modules'),
+    zip_safe=False)
