ae25b8c1180d17d40f52b1c46fd213e10f446d4e
Luis <luis@luisem.co>
2020-04-13 22:53:13 -0300

    Enable indicators

--- cyberhead/strategy.py
+++ cyberhead/strategy.py
@@ -5,7 +5,9 @@ import numpy as np
 import talib
 
 def trade(strategy):
-	print(strategy.timeseries)
+	ma1 = talib.SMA(strategy.timeseries.Close, 10)
+	ma2 = talib.SMA(strategy.timeseries.Close, 20)
+	print(ma1)
 
 st01 = CHStrategy('alpaca', 100000, 0.5, GOOG, 5, trade)
 st01.run()
